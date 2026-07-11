5ef5483cc1414ed17f3638a43704a9537e4d1113
pfyy <112098515+pfyy@users.noreply.github.com>
2026-07-11 21:48:04 +0800

    act50side

--- src/mods/chronosphere_win/main.py
+++ src/mods/chronosphere_win/main.py
@@ -22,6 +22,11 @@ def main():
     mgr.copy_zonemap_node()
 
     mgr.copy_merger_tree_node(
+        "activity/[uc]act50side/prefabs/zonemaps/zone_map_act50side_zone1",
+        "ui/zonemaps/zone_map_act50side_zone1",
+    )
+
+    mgr.copy_merger_tree_node(
         "activity/[uc]act24sre/zonemap/zone_map_act24sre_zone1",
         "ui/zonemaps/zone_map_act24sre_zone1",
     )
