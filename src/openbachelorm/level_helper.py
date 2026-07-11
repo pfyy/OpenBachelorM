81c40d0e2fbb396c0e9e891d1b3fc4d2165504e0
pfyy <112098515+pfyy@users.noreply.github.com>
2026-07-11 19:30:34 +0800

    new immune

--- src/openbachelorm/level_helper.py
+++ src/openbachelorm/level_helper.py
@@ -180,6 +180,11 @@ def get_codegen_migrate_level_decorator_lst():
 
 def handle_obj_in_level(obj, prts___levels):
     if isinstance(obj, prts___levels.clz_Torappu_EnemyDatabase_AttributesDataT):
+        if hasattr(obj, "teleportImmune") and obj.teleportImmune is None:
+            obj.teleportImmune = (
+                prts___levels.clz_Torappu_Undefinable_1_System_Boolean_T()
+            )
+
         if hasattr(obj, "palsyImmune") and obj.palsyImmune is None:
             obj.palsyImmune = prts___levels.clz_Torappu_Undefinable_1_System_Boolean_T()
 
