5a5a40c17a95403c3ce6c7d66b28f02fdd70f15b
pfyy <112098515+pfyy@users.noreply.github.com>
2026-07-11 19:11:54 +0800

    test win

--- test/test_known_table.py
+++ test/test_known_table.py
@@ -45,3 +45,12 @@ def test_known_table():
     load_known_table("2.7.31", "26-05-20-12-59-09_e8f456")
     load_known_table("2.7.41", "26-07-01-15-26-52_d3376d")
     load_known_table("2.7.51", "26-07-10-13-49-06_a14b4a")
+
+
+def test_known_table_windows():
+    load_known_table("2.6.91", "26-02-02-05-12-37_d6d557", "Windows")
+    load_known_table("2.7.01", "26-02-28-11-16-12_689e5e", "Windows")
+    load_known_table("2.7.11", "26-03-31-05-51-38_1cc8a5", "Windows")
+    load_known_table("2.7.21", "26-04-22-09-33-12_01a3a2", "Windows")
+    load_known_table("2.7.31", "26-05-20-14-05-50_24a03e", "Windows")
+    load_known_table("2.7.41", "26-07-01-15-25-07_7886d5", "Windows")
