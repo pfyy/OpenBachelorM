a85b18ffc1977432d57db29c6e8c2d40386da219
pfyy <112098515+pfyy@users.noreply.github.com>
2026-08-09 21:39:47 +0800

    init value

--- src/openbachelorm/const.py
+++ src/openbachelorm/const.py
@@ -82,11 +82,113 @@ class KnownTable(StrEnum):
 
 
 def get_last_res_version_android(client_version: str) -> str:
-    return ""
+    match client_version:
+        case "2.7.61":
+            return "26-08-07-14-53-29_30b8f0"
+        case "2.7.51":
+            return "26-07-20-09-52-12_5d4a43"
+        case "2.7.41":
+            return "26-07-01-15-26-52_d3376d"
+        case "2.7.31":
+            return "26-05-20-12-59-09_e8f456"
+        case "2.7.21":
+            return "26-04-22-10-22-20_1d417a"
+        case "2.7.11":
+            return "26-03-31-05-42-28_7d7f67"
+        case "2.7.01":
+            return "26-02-28-10-42-20_2bc282"
+        case "2.6.91":
+            return "26-02-02-04-52-41_58bd30"
+        case "2.6.82":
+            return "25-12-30-07-42-27_86bc9a"
+        case "2.6.71":
+            return "25-11-21-15-21-44_ee1197"
+        case "2.6.61":
+            return "25-10-23-13-35-37_3d4b91"
+        case "2.6.41":
+            return "25-09-28-12-13-16_6485b3"
+        case "2.6.21":
+            return "25-08-25-23-45-59_81c7ff"
+        case "2.6.01":
+            return "25-07-19-05-16-54_1e71a6"
+        case "2.5.80":
+            return "25-06-26-04-47-55_47709b"
+        case "2.5.60":
+            return "25-05-20-12-36-22_4803e1"
+        case "2.5.04":
+            return "25-04-25-08-42-16_acb2f8"
+        case "2.4.61":
+            return "25-03-27-16-19-10-4d4819"
+        case "2.4.41":
+            return "25-02-19-09-21-28-ba1f4e"
+        case "2.4.21":
+            return "25-01-08-07-44-44-3d8742"
+        case "2.4.01":
+            return "24-11-21-11-04-45-bae23b"
+        case "2.3.81":
+            return "24-10-24-14-30-30-b63a02"
+        case "2.3.61":
+            return "24-09-23-11-27-19-c6564b"
+        case "2.3.21":
+            return "24-08-26-16-10-17-fd946e"
+        case "2.3.01":
+            return "24-07-23-15-16-02-a53606"
+        case "2.2.81":
+            return "24-06-21-09-33-59-503529"
+        case "2.2.61":
+            return "24-05-22-06-44-01-4a3244"
+        case "2.2.41":
+            return "24-04-26-09-22-08-413e02"
+        case "2.2.21":
+            return "24-03-29-14-33-44-5002d2"
+        case "2.2.01":
+            return "24-02-26-08-28-19-0f351f"
+        case "2.1.41":
+            return "24-01-12-07-52-32-80033a"
+        case "2.1.21":
+            return "23-11-17-08-48-31-26d599"
+        case "2.1.01":
+            return "23-10-18-08-57-00-a2b96e"
+        case "2.0.81":
+            return "23-09-20-13-28-42-486799"
+        case "2.0.61":
+            return "23-08-25-11-36-41-12f55f"
+        case "2.0.40":
+            return "23-07-24-13-21-30-90fb63"
+        case "1.9.81":
+            return "23-03-27-11-50-38-b42880"
+        case "1.9.62":
+            return "23-02-22-22-37-04-ef2150"
+        case "1.9.42":
+            return "23-01-11-12-54-27-873b47"
+        case "1.8.81":
+            return "22-09-19-15-00-59-ae77d7"
+        case "1.8.61":
+            return "22-08-29-13-56-31-95a9a4"
+        case _:
+            raise ValueError(f"unknown client_version {client_version}")
 
 
 def get_last_res_version_windows(client_version: str) -> str:
-    return ""
+    match client_version:
+        case "2.7.61":
+            return "26-08-07-10-51-39_26e0fc"
+        case "2.7.51":
+            return "26-07-20-09-52-01_970b78"
+        case "2.7.41":
+            return "26-07-01-15-25-07_7886d5"
+        case "2.7.31":
+            return "26-05-20-14-05-50_24a03e"
+        case "2.7.21":
+            return "26-04-22-09-33-12_01a3a2"
+        case "2.7.11":
+            return "26-03-31-05-51-38_1cc8a5"
+        case "2.7.01":
+            return "26-02-28-11-16-12_689e5e"
+        case "2.6.91":
+            return "26-02-02-05-12-37_d6d557"
+        case _:
+            raise ValueError(f"unknown client_version {client_version}")
 
 
 def get_last_res_version(client_version: str, platform_name: str = "Android") -> str:
